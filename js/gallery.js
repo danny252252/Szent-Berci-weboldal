bd3478f03852615d1d279f21e49f898467989166
Dániel Beck <danny252252@gmail.com>
2023-09-18 21:32:23 +0200

    project ready

--- js/gallery.js
+++ js/gallery.js
@@ -8,3 +8,12 @@ imgs.forEach(img => {
     fullPage.style.display = 'block';
   });
 });
+
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
